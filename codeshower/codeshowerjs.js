5fb7a1499aa37b69d81e70b6c065b8c7ca4d0748
wilder <nadie437@gmail.com>
2021-05-19 10:41:39 -0500

    pintando palabras reservadas de js

--- codeshower/codeshowerjs.js
+++ codeshower/codeshowerjs.js
@@ -1,4 +1,52 @@
-const editor=document.getElementById('editable');
+const wordReserved = ["for", "while", "do", "if", "else", "switch","break", "case","default","try","catch", "throw","finally","return", "null", "continue", "new", "import", "export"]
+
+const jsReserved = ["undefined", "const", "let", "var", "function", "in", "instanceof", "this", "typeof","of", "async", "await"]
+
+const simbolReserved = ["=", "*","+","-","/","<", ">","(",")","{","}","[","]"]
+
+
+
+const codeTag = document.querySelector('.code-language')
+let codeText = codeTag.textContent;
+console.log("contenido: ",codeText);
+
+/**
+ * 
+ * @param {String} codeText 
+ * @param {String} reserved 
+ * @param {String} cssClass 
+ * @return {String} 
+ */
+const addTag = function(codeText, reserved, cssClass){
+  let regex = new RegExp(reserved,'g')
+  const tag = `<span class="${cssClass}">${reserved}</span>`
+  let codeTextReplaced = codeText.replace(regex, tag)  
+  return codeTextReplaced
+}
+
+const addTagForString = function(codeText, cssClass){
+  const exp = /([\'\"].*?[\'\"])/
+  let regex = new RegExp(exp,'g')
+  const tagForReplace = `<span class="${cssClass}">$1</span>`
+  let codeTextReplaced = codeText.replace(regex,tagForReplace)
+  console.log(codeTextReplaced);
+  return codeTextReplaced
+}
+
+for (const wordRes of wordReserved) {
+  codeText = addTag(codeText,wordRes,"reserved")
+}
+
+for (const jsword of jsReserved) {
+  codeText = addTag(codeText,jsword,"otro")
+}
+console.log(codeText);
+/* codeText = addTagForString(codeText, "string") */
+
+codeTag.innerHTML = codeText;
+
+/* ESlint */
+/* const editor=document.getElementById('editable');
 editor.addEventListener('keydown',function(event) {
   
   if (event.code==='Enter'){
@@ -6,4 +54,4 @@ editor.addEventListener('keydown',function(event) {
     editor.appendChild(c);
     c.focus();
   }
-});
+}); */
